0fcc9acc6c6eaf2c758e7e5a340045ffb9457c0f
David Isakson <ikeman32@yahoo.com>
2020-07-10 09:20:15 -0700

    ex2 working

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,8 +7,52 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
+    Given:
+    tickets = [
+    ticket_1 = Ticket("PIT", "ORD")
+        ticket_2 = Ticket("XNA", "SAP")
+        ticket_3 = Ticket("SFO", "BHM")
+        ticket_4 = Ticket("FLG", "XNA")
+        ticket_5 = Ticket("NONE", "LAX")
+        ticket_6 = Ticket("LAX", "SFO")
+        ticket_7 = Ticket("SAP", "SLC")
+        ticket_8 = Ticket("ORD", "NONE")
+        ticket_9 = Ticket("SLC", "PIT")
+        ticket_10 = Ticket("BHM", "FLG")]
+
+    return ["LAX", "SFO", "BHM", "FLG", "XNA", "CID", "SLC", "PIT", "ORD"]
+    where length = number of tickets
     """
     # Your code here
+    cache = {}
+    for t in tickets:
+        cache[t.source] = t.destination
 
+        # Your code here
+    route = []
+    trip = cache["NONE"]
+
+    while trip != "NONE":
+        route.append(trip)
+        trip = cache[trip]
+
+    route.append("NONE")
     return route
+
+
+if __name__ == "__main__":
+    ticket_1 = Ticket("PIT", "ORD")
+    ticket_2= Ticket("XNA", "SAP")
+    ticket_3= Ticket("SFO", "BHM")
+    ticket_4= Ticket("FLG", "XNA")
+    ticket_5= Ticket("NONE", "LAX")
+    ticket_6= Ticket("LAX", "SFO")
+    ticket_7= Ticket("SAP", "SLC")
+    ticket_8= Ticket("ORD", "NONE")
+    ticket_9= Ticket("SLC", "PIT")
+    ticket_10= Ticket("BHM", "FLG")
+        
+
+    tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+               ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+    print(reconstruct_trip(tickets, len(tickets)))
